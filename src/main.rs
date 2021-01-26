87d3888941546fb015c3b99621f7575a4283331f
Greg Schafer <greg.schafer@tangramvision.com>
2021-01-25 18:14:46 -0700

    Initial repo contents

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Tangram Robotics Inc. - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// ----------------------------
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let some_number = 2 + 2;
+        assert_eq!(some_number, 4);
+    }
+}
