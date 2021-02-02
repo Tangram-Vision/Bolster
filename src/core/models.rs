9477ae3a3f9590e5fc17d17c6329f25490449cab
Greg Schafer <greg.schafer@tangramvision.com>
2021-02-01 17:09:46 -0700

    Serialize to datetime type, not string

--- src/core/models.rs
+++ src/core/models.rs
@@ -3,6 +3,7 @@
 // Proprietary and confidential
 // ----------------------------
 
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -10,8 +11,8 @@ pub struct Dataset {
     /// Note: This is a Primary Key.<pk/>
     #[serde(rename = "uuid")]
     pub uuid: String,
-    #[serde(rename = "created_date")]
-    pub created_date: String,
+    #[serde(rename = "created_date", with = "notz_rfc_3339")]
+    pub created_date: DateTime<Utc>,
     #[serde(rename = "creator_role")]
     pub creator_role: String,
     #[serde(rename = "access_role")]
@@ -28,7 +29,7 @@ pub struct Dataset {
 impl Dataset {
     pub fn new(
         uuid: String,
-        created_date: String,
+        created_date: DateTime<Utc>,
         creator_role: String,
         access_role: String,
         url: String,
@@ -42,5 +43,44 @@ impl Dataset {
             url,
             metadata,
         }
+    }
+}
+
+// https://serde.rs/custom-date-format.html
+mod notz_rfc_3339 {
+    use chrono::{DateTime, TimeZone, Utc};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    const FORMAT: &str = "%Y-%m-%dT%H:%M:%S%.6f";
+
+    // The signature of a serialize_with function must follow the pattern:
+    //
+    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
+    //    where
+    //        S: Serializer
+    //
+    // although it may also be generic over the input types T.
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    // The signature of a deserialize_with function must follow the pattern:
+    //
+    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
+    //    where
+    //        D: Deserializer<'de>
+    //
+    // although it may also be generic over the output types T.
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT)
+            .map_err(serde::de::Error::custom)
     }
 }
